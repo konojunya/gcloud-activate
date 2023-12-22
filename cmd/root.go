065b8da99cff1febead8aab12753244a01b0b7fa
Junya Kono <junya03dance@gmail.com>
2023-12-22 19:43:23 +0900

    feat: info

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -9,6 +12,37 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "gcloud-activate",
 	Short: "gcloud-activate is gcloud cli alias",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		stdout, err := exec.Command("gcloud", "info").Output()
+		if err != nil {
+			return err
+		}
+
+		output := string(stdout)
+		line := strings.Split(output, "\n")
+		var account string
+		var projectId string
+
+		for _, v := range line {
+			if strings.Contains(v, "Account:") {
+				account = strings.TrimSpace(strings.Split(v, ":")[1])
+				account = strings.Trim(account, "[]")
+			}
+			if strings.Contains(v, "Project:") {
+				projectId = strings.TrimSpace(strings.Split(v, ":")[1])
+				projectId = strings.Trim(projectId, "[]")
+			}
+		}
+
+		if account == "" || projectId == "" {
+			fmt.Println(output)
+		}
+
+		fmt.Printf("Account: %s\n", account)
+		fmt.Printf("Project: %s\n", projectId)
+
+		return nil
+	},
 }
 
 func Execute() {
